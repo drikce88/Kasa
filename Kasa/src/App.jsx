4864e7996cb3b8afaafe8786cc4e68cbba6dff44
drikce88 <drikce88@gmail.com>
2023-10-26 16:58:01 +0200

    initialisation,création d'un systeme de routage

---
+++ Kasa/src/App.jsx
@@ -0,0 +1,14 @@
+import Routes from "./routes";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ Kasa/src/routes/index.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+const index = () => {
+    return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
+    );
+  };
+
+  export default index;
+  
