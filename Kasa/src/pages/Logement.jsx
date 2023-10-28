64abee43978eea840db2239edfebb58405e59c53
drikce88 <drikce88@gmail.com>
2023-10-28 13:30:37 +0200

    mise en place de routes fonctionnels vers les différentes pages de l'application. tests éffectués avec succé.

---
+++ Kasa/src/pages/Logement.jsx
@@ -0,0 +1,7 @@
+const Logement = () =>{
+    return(
+        <h1>Logement</h1>
+    )
+}
+
+export default Logement;
--- Kasa/src/routes/index.jsx
+++ Kasa/src/routes/index.jsx
@@ -1,11 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
+import Error from "../pages/Error";
+import Logement from "../pages/Logement";
+
 const index = () => {
     return (
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
+        <Route path="*" element={<Error />} />
+        <Route path="/Logement" element={<Logement />} />
       </Routes>
     );
   };
