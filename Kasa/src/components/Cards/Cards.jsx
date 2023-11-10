5c617cfffea42699ce005fa72214e854ade22896
drikce88 <drikce88@gmail.com>
2023-11-10 14:00:03 +0100

    Modification du composant Cards.jsx, utilisation de props comme demandé lors de la sesseion de mentorat.

--- Kasa/src/components/Cards/Cards.jsx
+++ Kasa/src/components/Cards/Cards.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
-import data from "../../assets/Location.json";
-import { Link } from "react-router-dom"
 
 
 
-export default function Cards() {
-    return (
-      <div className='card-container'>
-        {data.map((location) => (
-          <Link to={`/logement/${location.id}`} key={location.id}>
-            <div className='card'>
-              <img src={location.cover} alt={location.title} className='card-img' />
-              <p className='location-title'>{location.title}</p>
-            </div>
-          </Link>
-        ))}
-      </div>
-    );
+export default function Cards({ cover, title }) {
+	return (
+	  <article className="card">
+		<img className="card-img" src={cover} alt="location" />
+		<p className="location-title">{title}</p>
+	  </article>
+	);
   }
--- Kasa/src/pages/Home.jsx
+++ Kasa/src/pages/Home.jsx
@@ -2,6 +2,9 @@ import Header from "../components/Header/Header.jsx";
 import Banner from "../components/Banner/Banner.jsx";
 import Cards from "../components/Cards/Cards.jsx";
 import Footer from "../components/Footer/Footer.jsx";
+import data from "../assets/Location.json";
+import { Link } from "react-router-dom"
+
 
 
 
@@ -12,7 +15,13 @@ const Home = () =>{
         <>
         <Header />
         <Banner />
-        <Cards />
+        <div className="card-container">
+        {data.map((appart, id) => (
+  <Link className="link_card_logement" to={`/logement/${appart.id}`} key={id}>
+    <Cards cover={appart.cover} title={appart.title} />
+  </Link>
+))}
+      </div>
         <Footer />
         </>
       
