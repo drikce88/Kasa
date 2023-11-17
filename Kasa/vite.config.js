bd80919b1d55ea7f9abf8c4ce0af8bf41c781b04
drikce88 <drikce88@gmail.com>
2023-11-17 13:06:43 +0100

    réglage pour github pages

--- Kasa/vite.config.js
+++ Kasa/vite.config.js
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist', // Spécifiez le dossier de sortie ici
     rollupOptions: {
       external: ['react', 'react-dom', 'react-router-dom'],
     },
